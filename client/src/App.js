fa8c1e8f874a68ce3b418acc8f1f48f6b4b360fa
Pratik Temkar <pratiktemkarofficial@gmail.com>
2019-09-01 10:25:32 +0530

    Set Auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css';
 import Navbar from './components/layout/Navbar'
@@ -8,9 +8,20 @@ import Register from './components/auth/Register'
 import { Provider } from 'react-redux'
 import store from './store'
 import Alert from './components/layout/Alert'
+import Dashboard from './components/dashboard/Dashboard'
+import setAuthToken from './utils/setAuthToken'
+import { loadUser } from './actions/auth'
 
-const App = () =>
-  (
+if (localStorage.token) {
+  setAuthToken(localStorage.token)
+}
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser)
+  }, [])
+
+  return (
     <Provider store={store}>
       <Router>
         <Fragment>
@@ -21,12 +32,14 @@ const App = () =>
             <Switch>
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <Route exact path="/dashboard" component={Dashboard} />
             </Switch>
           </section>
         </Fragment>
       </Router>
     </Provider>
   )
+}
 
 
 export default App;
--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,8 +1,11 @@
 import React, { Fragment, useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, Redirect } from 'react-router-dom'
 import axios from 'axios'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import { login } from '../../actions/auth'
 
-const Login = () => {
+const Login = ({ login, isAuthenticated }) => {
     const [formData, setFormData] = useState({
         email: '',
         password: ''
@@ -12,8 +15,13 @@ const Login = () => {
     const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value })
     const onSubmit = async e => {
         e.preventDefault()
-        console.log('Success')
+        login(email, password)
 
+    }
+
+    // redirect if logged in
+    if (isAuthenticated) {
+        return <Redirect to="/dashboard" />
     }
 
     return (
@@ -43,4 +51,13 @@ const Login = () => {
     )
 }
 
-export default Login
+Login.propTypes = {
+    login: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool
+}
+
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated
+})
+
+export default connect(mapStateToProps, { login })(Login)
